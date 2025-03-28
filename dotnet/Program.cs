8e43e96dc93b2480d3dd442681dc5feb3b1a18b0
Antoine Chéneau <antoine.cheneau@outlook.com>
2025-03-28 14:29:41 +0100

    Traitement d'image locale

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,32 +1,36 @@
 ﻿using System;
-using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using System.IO;
 
-namespace dotnet
+namespace ImageProcessingApp
 {
-    class Personne
-    {
-        public string nom { get; set; }
-        public int age { get; set; }
-
-        public string Hello(bool isLowercase)
-        {
-            string message = $"hello {nom}, you are {age}";
-            return isLowercase ? message.ToLower() : message.ToUpper();
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Personne personne = new Personne
-            {
-                nom = "Alice",
-                age = 30
-            };
+            string inputPath = @"img.jpeg";
+            string outputPath = @"img-resized.jpeg";
 
-            string json = JsonConvert.SerializeObject(personne, Formatting.Indented);
-            Console.WriteLine(json);
+            RedimensionnerImage(inputPath, outputPath);
+        }
+
+        static void RedimensionnerImage(string inputPath, string outputPath)
+        {
+            try
+            {
+                using (Image image = Image.Load(inputPath))
+                {
+                    image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+                    image.Save(outputPath, new JpegEncoder());
+                    Console.WriteLine($"Image redimensionnée et sauvegardée à : {outputPath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors du traitement de l'image : {ex.Message}");
+            }
         }
     }
 }
